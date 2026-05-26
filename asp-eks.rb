9af0af880d32831c70a410f3e5f1cd87534f34be
Eimar Fandino <eimarfandino@outlook.com>
2026-05-26 17:35:24 +0200

    upgrade to version 0.0.5

--- asp-eks.rb
+++ asp-eks.rb
@@ -1,9 +1,9 @@
 class AspEks < Formula
     desc "AWS profile switcher for EKS CLI access"
     homepage "https://github.com/eimarfandino/asp-eks"
-    url "https://github.com/eimarfandino/asp-eks/releases/download/v0.0.2/asp-eks_0.0.2_darwin_arm64.zip"
+    url "https://github.com/eimarfandino/asp-eks/releases/download/v0.0.5/asp-eks_0.0.5_darwin_arm64.zip"
     sha256 "41ef7d6f26f7c5c3eb8f10512473ca800843c5669c1c8d5bc365ede06c49dbc3"
-    version "0.0.2"
+    version "0.0.5"
   
     def install
       bin.install "asp-eks"
