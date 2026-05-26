ba24a38bce3a0a2b11810b078fd0718fa6e45504
Eimar Fandino <eimarfandino@outlook.com>
2026-05-26 17:43:22 +0200

    ficx checksum

--- asp-eks.rb
+++ asp-eks.rb
@@ -2,7 +2,7 @@ class AspEks < Formula
     desc "AWS profile switcher for EKS CLI access"
     homepage "https://github.com/eimarfandino/asp-eks"
     url "https://github.com/eimarfandino/asp-eks/releases/download/v0.0.5/asp-eks_0.0.5_darwin_arm64.zip"
-    sha256 "e12c4d2a35c7857db155e6875643192c1b30ad41d65eb7d89744ed67b711069e"
+    sha256 "099c807178b1396f9526aaf9c9e9b6142eb924a6d22abae7a3d18d1e711bfdc5"
     version "0.0.5"
   
     def install
