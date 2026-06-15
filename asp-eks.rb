17f082d675d50362c5e894991546ccd56b5899a7
Eimar Fandino Hurtado <eimar.fandinohurtado@asnbank.nl>
2026-06-15 10:24:41 +0200

    chore: update asp-eks

--- asp-eks.rb
+++ asp-eks.rb
@@ -2,7 +2,7 @@ class AspEks < Formula
     desc "AWS profile switcher for EKS CLI access"
     homepage "https://github.com/eimarfandino/asp-eks"
     url "https://github.com/eimarfandino/asp-eks/releases/download/v0.1.0/asp-eks_0.1.0_darwin_arm64.tar.gz"
-    sha256 "3750f50493fdf081058fb619572a5d24908b5e153789d0ba1fe0fdfe80c27ef5"
+    sha256 "3e4f84976593ff4b07894fd308dc1d5950715c12ea779ef13453343aa845741e"
     version "0.1.0"
 
     def install
